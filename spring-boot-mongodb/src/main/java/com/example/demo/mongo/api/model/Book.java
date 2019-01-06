ba28ab6aa4a58aae243178738e7cc806ac12a9cc
SachinMittal101 <sam.mittal.101@gmail.com>
2019-01-06 21:23:38 +0530

    Add files via upload

---
+++ spring-boot-mongodb/src/main/java/com/example/demo/mongo/api/model/Book.java
@@ -0,0 +1,43 @@
+package com.example.demo.mongo.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Document(collection = "Book")
+public class Book {
+
+	@Id
+	private Integer id;
+	private String bookName;
+	private String authorName;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", bookName=" + bookName + ", authorName=" + authorName + "]";
+	}
+	
+	
+	
+}
